3958b52699dd3b9e1798e6b696112eae79654d70
Antonio Piedra Pacheco <anto.piedrapach@hotmail.com>
2023-08-28 14:48:02 -0600

    Improvement and exception management in parameter reader

--- lectorParametros.py
+++ lectorParametros.py
@@ -1,5 +1,7 @@
 import sys
 import getopt
+import json
+import re
 
 class LectorParametros:
 
@@ -7,37 +9,33 @@ class LectorParametros:
         self.argumentList = sys.argv[1:]
         self.options = "d:c:t:s:h:"
         self.longOptions = ["dataset=","cross-validation=","tipo=","split=", "colNA=","const=", "corr=", "cvThreshold="]
-    
+        opcionesLector = json.load(open("parametrosLineaComando.json","r"))
+        self.parameterMap = opcionesLector["llaves"]
+        self.defaultValues = opcionesLector["valores_default"]
+
     def leerParametros(self):
         parametros, valores = getopt.getopt(self.argumentList, self.options, self.longOptions)
         diccionarioValores = {}
-        splitFlag = False
-        porcentajeSplit = 0.15
-        constThreshold = 0.8
-        corrThreshold = 0.95
-        colNAThreshold = 0
         for parametroActual, valorActual in  parametros:
-            if parametroActual in ("-d","--dataset"):
-                diccionarioValores["datos"] = valorActual
-            elif parametroActual in ("-c","--cross-validation"):
-                diccionarioValores["validacion"] = valorActual
-            elif parametroActual in ("-t","--tipo"):
-                diccionarioValores["tipo"] = valorActual
-            elif parametroActual in ("-s","--split"):
-                splitFlag = True
-                porcentajeSplit = float(valorActual)
-            elif parametroActual == "--const":
-                diccionarioValores["constThreshold"] = float(valorActual)
-            elif parametroActual == "--corr":
-                diccionarioValores["corrThreshold"] = float(valorActual)
-            elif parametroActual in ("-h","--cvThreshold"):
-                diccionarioValores["cvThreshold"] = float(valorActual)
-            elif parametroActual == "--colNA":
-                colNAThreshold = float(valorActual)
-        diccionarioValores["split"] = splitFlag
-        diccionarioValores["porcentajeSplit"] = porcentajeSplit
-        diccionarioValores["constThreshold"] = constThreshold
-        diccionarioValores["corrThreshold"] = corrThreshold
-        diccionarioValores["colNAThreshold"] = colNAThreshold
+            llaveActual = self.parameterMap[parametroActual]
+            if llaveActual not in diccionarioValores:
+                isNumber = re.search("^[0-9]+.?[0-9]*$",valorActual)
+                if isNumber:
+                    if not "." in valorActual:
+                        valorActual = int(valorActual)
+                    else:
+                        valorActual = float(valorActual)
+                diccionarioValores[llaveActual] = valorActual
+                if llaveActual == "porcentajeSplit":
+                    diccionarioValores["split"] = True
+            else:
+                raise Exception(f"Este argumento ya fue utilizado: {parametroActual}")
+        for parametro in self.parameterMap:
+            llave = self.parameterMap[parametro]
+            if llave not in diccionarioValores:
+                if llave in self.defaultValues:
+                    diccionarioValores[llave] = self.defaultValues[llave]
+                else:
+                    diccionarioValores[llave] = None
         return diccionarioValores
 
